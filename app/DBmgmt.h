34b504ab6c05dee85cdb1d136ee3b1958fcbff27
Matteo Marino <95318540+Mathieus99@users.noreply.github.com>
2023-11-10 14:18:14 +0100

    Finita libreria DB (no test)

--- app/DBmgmt.h
+++ app/DBmgmt.h
@@ -8,12 +8,14 @@
     #define DATABASE "my_database"
     #define COLLECTION "my_collection"
 
-    mongoc_client_t *DBConnect(const char *uri);        //Connection to DB
-    mongoc_database_t *get_database(mongoc_client_t *client, const char *database);
-    mongoc_collection_t *get_collection(mongoc_database_t *database, const char *collection);
-    bson_t *load_json(const char *json);
+    mongoc_client_t *DBConnect();                               //Crea la connessione a MongoDB
+    mongoc_database_t *get_database(mongoc_client_t *client);   //Crea la connessione col database
+    mongoc_collection_t *get_collection();                      //Crea la connessione con la collezione
+    bson_t *load_json(const char *json);                        //Converte il JSON in BSON per MongoDB
+    void caricaJSON(const char *json);                          //Carica il BSON nella collezione
+    void DBDisconnect();                                        //Chiude la connessione
 
-    //TODO: DB Connection
+    //Crea la connessione con MongoDB
     void DBConnect(){
         // Crea un client MongoDB
         mongoc_client_t *client = mongoc_client_new(MONGODB_URI);
@@ -27,7 +29,8 @@
         return client;
     }
     
-    mongoc_database_t *get_database(mongoc_client_t *client, const char *database){
+    //Crea la connessione con il DB
+    mongoc_database_t *get_database(mongoc_client_t *client){
         // Crea una connessione al database
         mongoc_database_t *database = mongoc_client_get_database(client, DATABASE);
 
@@ -40,7 +43,8 @@
         return database;
     }
 
-    mongoc_collection_t *get_collection(mongoc_database_t *database, const char *collection){
+    //Crea una connessione con la collezione
+    mongoc_collection_t *get_collection(){
         // Crea una collezione
         mongoc_collection_t *collection = mongoc_database_get_collection(DATABASE, COLLECTION);
 
@@ -53,6 +57,7 @@
         return collection;
     }
 
+    //Converte il JSON in BSON per MongoDB
     bson_t *load_json(const char *json){
         // Carica un JSON
         bson_t *document = bson_new_from_json(json, strlen(json));
@@ -66,13 +71,27 @@
         return document;
     }
 
-    //TODO: Caricamento JSON    
-    void caricaJSON(){
-
+    //Carica il JSON nella collezione    
+    void caricaJSON(const char *json){
+        mongoc_client_t *client = DBConnect();
+        mongoc_database_t *database = get_database(client);
+        mongoc_collection_t *collection = get_collection();
+        bson_t *device = load_json(json);
+        mongoc_collection_insert_one(collection, device, NULL, NULL, &error);
+        if(error){
+            fprintf(stderr, "Errore: %s\n", error.message);
+            mongoc_error_cleanup(&error);
+            return EXIT_FAILURE;
+        }
+        DBDisconnect(client,database,collection,document);
     }
 
-    void DBDisconnect(){
-
+    //Chiude la connessione
+    void DBDisconnect(mongoc_client_t *client,mongoc_collection_t *collection, mongoc_database_t *db,bson_t *doc){
+        bson_free(doc);
+        mongoc_collection_destroy(collection);
+        mongoc_database_destroy(db);
+        mongoc_client_destroy(client);
     }
 
 #endif
